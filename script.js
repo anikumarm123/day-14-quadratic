831a5068f420e547bba81af3ead85434a658fa3f
muthukumar <anikumar2825@gmail.com>
2022-06-24 10:18:06 +0530

    add-file.

---
+++ script.js
@@ -0,0 +1,13 @@
+//quadratic equation//
+let a = 2, b= 3, c = -7 ;
+ 
+d = b * b - 4 * a * c;
+
+q = Math.sqrt(d);
+console.log(q)
+q1 = (-b + q) / (2 * a);
+
+q2 = (-b - q) / (2 * a);
+
+console.log(q1,q2);
+
